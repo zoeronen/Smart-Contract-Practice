6618e2d96bde74c3903e71c7768615b8c8e0de14
Zoe Ronen <zoeronen@email.com>
2022-06-06 14:16:37 -0400

    simple fundme kovan

---
+++ FundMeSimple.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: LGPL-2.0-or-higher
+
+// Smart contract that lets anyone deposit ETH into the contract
+// Only the owner of the contract can withdraw the ETH
+pragma solidity ^0.6.0;
+
+// Get the latest ETH/USD price from chainlink price feed
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        addressToAmountFunded[msg.sender] += msg.value;
+        // ETH -> USD conversion rate
+    }
+
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        return priceFeed.version();
+    }
+
+}
