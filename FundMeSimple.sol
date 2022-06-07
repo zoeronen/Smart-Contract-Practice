af7ef96e0facbe3f737b5c9be38668c433653b3f
Zoe Ronen <zoeronen@email.com>
2022-06-06 22:31:39 -0400

    simplify

--- FundMeSimple.sol
+++ FundMeSimple.sol
@@ -13,12 +13,5 @@ contract FundMe {
 
     function fund() public payable {
         addressToAmountFunded[msg.sender] += msg.value;
-        // ETH -> USD conversion rate
     }
-
-    function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
-        return priceFeed.version();
-    }
-
 }
