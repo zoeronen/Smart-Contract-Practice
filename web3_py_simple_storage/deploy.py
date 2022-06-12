cefd1c8b40addd9c15027010ef6941c92d227bae
= <=>
2022-06-12 11:47:46 -0400

    Web3.py full ganache-cli deploy

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -42,9 +42,9 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # Connect to ganache
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))  # 7545 for Ganache GUI
 chain_id = 1337
-my_address = "0xC209ab91Ab54952FCc055B6adB5FE99c8e3cBBB0"
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
 private_key = os.getenv(
     "PRIVATE_KEY"
 )  # get private key from env! do not hard code here!
@@ -68,11 +68,39 @@ print(transaction)
 
 # 2. Sign a transaction
 signed_txt = w3.eth.account.sign_transaction(transaction, private_key=private_key)
-print("Deploying Contract!")
 
 # 3. Send a transaction
+print("Deploying Contract!")
 tx_hash = w3.eth.send_raw_transaction(signed_txt.rawTransaction)
 # Wait for the transaction to be mined, and get the transaction receipt
 print("Waiting for transaction to finish...")
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
+
+# Working with deployed contract:
+# Contract Address (address=tx_receipt.contractAddress)
+# Contract ABI (abi=abi)
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+## Call     -> Simulate making the call and getting a return value
+## Transact -> Actually making a state change to the blockchain
+
+# Initial value of favorite number:
+print(simple_storage.functions.retrieve().call())
+print("Updating Contract!")
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+print("Updating stored Value...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated!")
+print(simple_storage.functions.retrieve().call())
