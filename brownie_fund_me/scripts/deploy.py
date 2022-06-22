72441000c891acc06ed4883ecea7ff6591ad3a1c
Zoe Ronen <zoeronen@email.com>
2022-06-22 19:18:24 -0400

    brownie fund me pretty deploy

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,5 +1,9 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 
 
 def deploy_fund_me():
@@ -8,18 +12,13 @@ def deploy_fund_me():
 
     # if we are on a persistent network, use the associated address
     # otherwise, deploy mocks
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
     else:
-        print(f"The active network is {network.show_active()}")
-        print("Deploying Mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(
-            18, 2000000000000000000000, {"from": account}
-        )
-        price_feed_address = mock_aggregator.address
-        print("Mocks Deployed!")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed_address,
