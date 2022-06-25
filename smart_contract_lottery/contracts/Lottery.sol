5b05f21dc9a32a74d971893b791f12a4f43d9360
Zoe Ronen <zoeronen@email.com>
2022-06-25 19:23:05 -0400

    lottery test entrance fee

--- smart_contract_lottery/contracts/Lottery.sol
+++ smart_contract_lottery/contracts/Lottery.sol
@@ -1,9 +1,34 @@
+// SPDX-License-Identifier: LGPL-2.0-or-later
 pragma solidity ^0.6.6;
 
-contract Lottery {
-    function enter() public {}
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
-    function getEntranceFee() public {}
+contract Lottery {
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    function enter() public payable {
+        // $50 minimum
+
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        //
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+        // $50, $2,000 / ETH
+        // 50/2,000
+        // 50 * 100000 / 2000
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
 
     function startLottery() public {}
 
