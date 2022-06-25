eaddbfaaeec66548d7d090239e1f78eb8b34f2e4
Zoe Ronen <zoeronen@email.com>
2022-06-25 11:32:52 -0400

    lottery layout

---
+++ smart_contract_lottery/contracts/Lottery.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Lottery {
+    function enter() public {}
+
+    function getEntranceFee() public {}
+
+    function startLottery() public {}
+
+    function endLottery() public {}
+}
