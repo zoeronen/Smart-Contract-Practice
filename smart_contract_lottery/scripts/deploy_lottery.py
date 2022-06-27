4908fb8cc83a712a2bab91cc0e008162e2eae10e
Zoe Ronen <zoeronen@email.com>
2022-06-27 18:27:40 -0400

    lottery deploy local with mocks

---
+++ smart_contract_lottery/scripts/deploy_lottery.py
@@ -0,0 +1,20 @@
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import Lottery, config, network
+
+
+def deploy_lottery():
+    account = get_account(id="sample-account")
+    lottery = Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
+    )
+    print("Deployed lottery!")
+
+
+def main():
+    deploy_lottery()
